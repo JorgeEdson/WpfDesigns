c88a15af358079cb66d24d95c041740278813323
Jorge Edson <jorge.edson.bsi@gmail.com>
2023-04-12 02:16:45 -0300

    add new example dashboardAdmin

---
+++ DashboardAdmin/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DashboardAdmin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void ChangeWindowState() 
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+                WindowEdge.Visibility = Visibility.Collapsed;
+            }
+            else 
+            {
+                if (WindowState == WindowState.Maximized) 
+                {
+                    this.WindowState = WindowState.Normal;
+                    WindowEdge.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        private void MainWindowPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left && e.ClickCount == 2) 
+            {
+                ChangeWindowState();
+            }
+        }
+    }
+}
